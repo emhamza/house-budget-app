5733a3025fe9e849568e8ec412e24d84892a5d17
emhamza <imumairhamza@gmail.com>
2023-08-09 21:20:10 +0500

    Add views file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :items, dependent: :destroy, foreign_key: :author_id
   has_many :balances, dependent: :destroy, foreign_key: :author_id
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  
   validates :name, presence: true
 end
